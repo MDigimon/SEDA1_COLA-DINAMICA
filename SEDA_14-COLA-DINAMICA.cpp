5d5395ce7d0f8e7b2f0248413fee079b6e1c4f1c
Jesus Eduardo Quintero Gomez <71619810+MDigimon@users.noreply.github.com>
2020-11-16 23:36:29 -0600

    Terminado

--- SEDA_14-COLA-DINAMICA.cpp
+++ SEDA_14-COLA-DINAMICA.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <stdlib.h>
-
+#include <sstream>
 
 using namespace std;
 
@@ -8,7 +8,8 @@ class Nodo{
 	public:
 		int valor;
 		Nodo* next;
-}
+};
+
 
 int vacia(Nodo* frente){
 	if(frente == NULL){
@@ -40,4 +41,109 @@ void eliminar(Nodo *&frente, Nodo *&fin){
 		frente = frente->next;
 	}
 	delete(aux);
+}
+
+void mostrar(Nodo *&frente, int cont){
+	Nodo *temp;
+	temp = frente;
+	cout << endl;
+	while(frente != NULL){
+		cout << "\t" << frente->valor;
+		temp = temp->next;
+	}
+	cout << endl;
+}
+
+void tope(Nodo *&frente, Nodo *&fin, int n){
+	cout << "Primer Dato: " << frente->valor << endl;
+	cout << "Ultimo Dato: " << fin->valor << endl << endl;
+	
+}
+
+int cambio(int n){
+	if(n == 1){
+		return 0;
+	}
+	return 1;
+}
+
+
+int capturar(){
+	string dato;
+	int num, i;
+	
+	cout << "Nuevo Elemento: ";
+	cin >> dato;
+	
+	for(i=0; i < dato.size(); i++){
+		if(isdigit(dato[i]) != 0){
+			if((int)dato[i] != 0){
+				cout << "DATO INCORRECTO" << endl;
+				system("pause");
+				system("cls");
+				return -1;
+			}
+		}		
+	}
+	
+	istringstream(dato) >> num;
+	return num;
+}
+
+
+
+
+
+int main(){
+	string opc;
+	int num, f=1, m=1, t=1, cont = 0, cont2 = 0;
+	Nodo *frente = NULL,*fin = NULL;
+	
+	while(f==1){
+		if(m == 1){
+			mostrar(frente, cont);
+			cout << endl;
+		}
+		if(t == 1){
+			tope(frente, fin, cont);
+			cout << endl;
+		}
+		cout << "----------Seleccione una Opcion---------" << endl;
+		cout << "1) Agregar" << endl;
+		cout << "2) Eliminar" << endl;
+		cout << "3) Mostrar Tope" << endl;
+		cout << "4) Mostrar Pila" << endl;
+		cout << "5) Salir" << endl;
+		cin >> opc;
+		
+		if(opc == "1"){
+			num = capturar();
+			if(num != -1){
+				cont++;
+				agregar(frente, fin, num);
+			}
+			
+		}
+		else if(opc == "2"){
+			eliminar(frente, fin);
+			cont--;
+		}
+		else if(opc == "3"){
+			t = cambio(t);
+		}
+		else if(opc == "4"){
+			m = cambio(m);
+		}
+		else if(opc == "5"){
+			system("cls");
+			cout << "FIN DEL PROGRAMA" << endl;
+		}
+		else{
+			cout << "Opcion Incorrecta" << endl;
+			system("pause");
+			system("cls");
+		}	
+	}
+	
+	return 0;
 }
